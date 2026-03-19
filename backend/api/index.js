cc0449e666535fba6d1bdcee911d326f5e067840
Reem Mousa <reemelshamly@gmail.com>
2026-03-19 23:24:49 +0200

    Add explicit CORS headers for Vercel serverless

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,15 @@
 import app from '../server.js'
 
+// Add CORS middleware for Vercel
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200)
+    }
+    next()
+})
+
 export default app
